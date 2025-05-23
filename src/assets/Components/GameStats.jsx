8402411481b7a29ad7a40401a3756cfaad4452c8
atomy <atomy@jackinpoint.net>
2025-05-23 23:58:11 +0200

    - stuff

--- src/assets/Components/GameStats.jsx
+++ src/assets/Components/GameStats.jsx
@@ -1,10 +1,22 @@
 import "./GameStats.css";
+import { GiPoliceOfficerHead } from "react-icons/gi";
 
-export function GameStats({correctCheckedPassport, faultyCheckedPassport, remainingPassports}) {
-    return (
-        <div className="Game_Stats_Box">
-            <p>Passports checked: {correctCheckedPassport + faultyCheckedPassport}</p>
-            <p>Passports to be checked: {remainingPassports}</p>
-        </div>
-    );
+export function GameStats({
+  correctCheckedPassport,
+  faultyCheckedPassport,
+  remainingPassports,
+}) {
+  return (
+    <div className="Game_Stats_Box">
+      <div className="Stats_Icon">
+        <GiPoliceOfficerHead className="Officer_Icon" />
+      </div>
+      <div className="Stats_Text">
+        <p>
+          Passports checked: {correctCheckedPassport + faultyCheckedPassport}
+        </p>
+        <p>Passports to be checked: {remainingPassports}</p>
+      </div>
+    </div>
+  );
 }
