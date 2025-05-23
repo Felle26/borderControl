83e3f85d676cc6c7d4291b4457b6f583cf1cbae4
atomy <atomy@jackinpoint.net>
2025-05-23 21:42:26 +0200

    - re-add button

--- src/assets/Components/PassPort/Passport.jsx
+++ src/assets/Components/PassPort/Passport.jsx
@@ -1,45 +1,47 @@
 import "./Passport.css";
-import {PassportFrontside} from "./PassportFrontside.jsx";
-import {PassportBackside} from "./PassportBackside.jsx";
-import {useState} from "react";
-import {MdFlipCameraAndroid} from "react-icons/md";
+import { PassportFrontside } from "./PassportFrontside.jsx";
+import { PassportBackside } from "./PassportBackside.jsx";
+import { useState } from "react";
+import { MdFlipCameraAndroid } from "react-icons/md";
 
-export function Passport({PassportData, isHighlighted = true}) {
-    const [isFrontSideVisible, setIsFrontSideVisible] = useState(true);
+export function Passport({ PassportData, isHighlighted = true }) {
+  const [isFrontSideVisible, setIsFrontSideVisible] = useState(true);
 
-    const handleFlip = () => {
-        setIsFrontSideVisible(!isFrontSideVisible);
-    };
+  const handleFlip = () => {
+    setIsFrontSideVisible(!isFrontSideVisible);
+  };
 
-    return (
-        <>
-            {/*<div*/}
-            {/*    className={`passport_highlight_wrapper ${*/}
-            {/*        isHighlighted ? "highlighted" : ""*/}
-            {/*    }`}*/}
-            {/*>*/}
-                <div className="Passport">
-                    {isFrontSideVisible && (
-                        <PassportFrontside PassportData={PassportData}/>
-                    )}
-                    {!isFrontSideVisible && (
-                        <PassportBackside PassportData={PassportData}/>
-                    )}
-                </div>
-            {/*</div>*/}
-            {/*<button*/}
-            {/*    // onClick={() => handleSelectPassport(index)}*/}
-            {/*    // aria-label={`Select passport ${index + 1}`}*/}
-            {/*/>*/}
-            <button
-                className="Flip_Button"
-                onClick={handleFlip}
-                aria-label={isFrontSideVisible ? "Show back side" : "Show front side"}
-            >
-                <MdFlipCameraAndroid
-                    className={`Flip_Icon ${!isFrontSideVisible ? "Flipped" : ""}`}
-                />
-            </button>
-        </>
-    );
+  return (
+    <>
+      {/*<div*/}
+      {/*    className={`passport_highlight_wrapper ${*/}
+      {/*        isHighlighted ? "highlighted" : ""*/}
+      {/*    }`}*/}
+      {/*>*/}
+      <button>
+        <div className="Passport">
+          {isFrontSideVisible && (
+            <PassportFrontside PassportData={PassportData} />
+          )}
+          {!isFrontSideVisible && (
+            <PassportBackside PassportData={PassportData} />
+          )}
+        </div>
+      </button>
+      {/*</div>*/}
+      {/*<button*/}
+      {/*    // onClick={() => handleSelectPassport(index)}*/}
+      {/*    // aria-label={`Select passport ${index + 1}`}*/}
+      {/*/>*/}
+      <button
+        className="Flip_Button"
+        onClick={handleFlip}
+        aria-label={isFrontSideVisible ? "Show back side" : "Show front side"}
+      >
+        <MdFlipCameraAndroid
+          className={`Flip_Icon ${!isFrontSideVisible ? "Flipped" : ""}`}
+        />
+      </button>
+    </>
+  );
 }
