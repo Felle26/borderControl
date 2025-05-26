5263e4d66c5238eb2ed7373d329b52d816cafa43
Felle26 <sebastianfelsberg@web.de>
2025-05-26 22:21:51 +0200

    fix: Update StoryBoard component to handle empty state

--- src/assets/Components/Storyboard/StoryBoard.comp.jsx
+++ src/assets/Components/Storyboard/StoryBoard.comp.jsx
@@ -5,6 +5,8 @@ export function StoryBoard({StoryBoardData}) {
     <div className="Story_Board_container">
       <h2>Find the Matching Passport</h2>
       <p>Hello Officer, find the Right Passport</p>
+      <p>Follow these steps to complete your task:</p>
+      <p>Today is 2025/05/26 </p>
       <ul>
         {StoryBoardData.steps.map((step, index) => (
           <li key={index}>
