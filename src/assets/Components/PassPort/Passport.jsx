07b04d7f99096f46985ccc60e2ab85f1794a1b16
atomy <atomy@jackinpoint.net>
2025-05-26 22:11:55 +0200

    - mute now also prevents flip sounds from playing

--- src/assets/Components/PassPort/Passport.jsx
+++ src/assets/Components/PassPort/Passport.jsx
@@ -1,53 +1,51 @@
 import "./Passport.css";
-import {PassportFrontside} from "./PassportFrontside.jsx";
-import {PassportBackside} from "./PassportBackside.jsx";
-import {useEffect, useState} from "react";
-import {MdFlipCameraAndroid} from "react-icons/md";
-import {FaCheckCircle} from "react-icons/fa";
+import { PassportFrontside } from "./PassportFrontside.jsx";
+import { PassportBackside } from "./PassportBackside.jsx";
+import { useEffect, useState } from "react";
+import { MdFlipCameraAndroid } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import audioService from "../../AudioService";
 
-export function Passport({PassportData, handleSubmit}) {
-    const [isFrontSideVisible, setIsFrontSideVisible] = useState(true);
+export function Passport({ PassportData, handleSubmit }) {
+  const [isFrontSideVisible, setIsFrontSideVisible] = useState(true);
 
-    let turnPassport = new Audio("./Sounds/card-shove-1.ogg");
-    turnPassport.volume = 0.1;
+  const handleFlip = () => {
+    setIsFrontSideVisible(!isFrontSideVisible);
+    audioService.playSound("passportFlip", "./Sounds/card-shove-1.ogg");
+  };
 
-    const handleFlip = () => {
-        setIsFrontSideVisible(!isFrontSideVisible);
-        turnPassport.play();
-    };
+  useEffect(() => {
+    // console.log("current passport data: " + PassportData)
+  }, [PassportData]);
 
-    useEffect(() => {
-        // console.log("current passport data: " + PassportData)
-    }, [PassportData]);
-
-    return (
-        <>
-            <button
-                className="Submit_Button"
-                onClick={() => handleSubmit(PassportData)}
-                aria-label="Select passport for choice"
-            >
-                <FaCheckCircle
-                    className={`Flip_Icon ${!isFrontSideVisible ? "Flipped" : ""}`}
-                />
-            </button>
-            <div className="Passport">
-                {isFrontSideVisible && (
-                    <PassportFrontside PassportData={PassportData}/>
-                )}
-                {!isFrontSideVisible && (
-                    <PassportBackside PassportData={PassportData}/>
-                )}
-            </div>
-            <button
-                className="Flip_Button"
-                onClick={handleFlip}
-                aria-label={isFrontSideVisible ? "Show back side" : "Show front side"}
-            >
-                <MdFlipCameraAndroid
-                    className={`Flip_Icon ${!isFrontSideVisible ? "Flipped" : ""}`}
-                />
-            </button>
-        </>
-    );
+  return (
+    <>
+      <button
+        className="Submit_Button"
+        onClick={() => handleSubmit(PassportData)}
+        aria-label="Select passport for choice"
+      >
+        <FaCheckCircle
+          className={`Flip_Icon ${!isFrontSideVisible ? "Flipped" : ""}`}
+        />
+      </button>
+      <div className="Passport">
+        {isFrontSideVisible && (
+          <PassportFrontside PassportData={PassportData} />
+        )}
+        {!isFrontSideVisible && (
+          <PassportBackside PassportData={PassportData} />
+        )}
+      </div>
+      <button
+        className="Flip_Button"
+        onClick={handleFlip}
+        aria-label={isFrontSideVisible ? "Show back side" : "Show front side"}
+      >
+        <MdFlipCameraAndroid
+          className={`Flip_Icon ${!isFrontSideVisible ? "Flipped" : ""}`}
+        />
+      </button>
+    </>
+  );
 }
